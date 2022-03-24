d752b950a0669f851982f22476fd39621239896b
Blueline0425 <happyhj6@gachon.ac.kr>
2022-03-24 18:38:41 +0900

    univ_assignments

---
+++ vscode/d.py
@@ -0,0 +1,20 @@
+a={1,2,3,4,5,6,7,8,9}
+import random
+
+def c():
+    for l in a:
+     for k in a:
+        if k>l:
+            print("k is lager than l")
+        else:
+            print("k is small than l")
+c()
+
+
+
+
+
+
+
+
+
