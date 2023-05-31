f9eef37a8f938e282fadf1ac9763bbe564c8ec9c
Blueline0425 <83590303+Blueline0425@users.noreply.github.com>
2023-05-31 23:50:57 +0900

    Create 10813 공 바꾸기.java

---
+++ UNIV_FRESHMAN_ASSIGNMENTS/BaekJoon/단계별로풀어보기/1차원 배열/10813 공 바꾸기.java
@@ -0,0 +1,28 @@
+import static java.lang.System.out;
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int m = scanner.nextInt();
+		int[] narr=new int[n];
+		for(int i=0;i<n;i++){
+		    narr[i]=i+1;
+		}
+		
+		for(int a=0;a<m;a++){
+		    int i,j;
+		    i=scanner.nextInt();
+		    j=scanner.nextInt();
+		    int tmp;
+		    
+		    tmp = narr[i-1];
+		    narr[i-1]=narr[j-1];
+		    narr[j-1]=tmp;
+		}
+		for(int i:narr)
+		    out.printf("%d ",i);
+	}
+}
