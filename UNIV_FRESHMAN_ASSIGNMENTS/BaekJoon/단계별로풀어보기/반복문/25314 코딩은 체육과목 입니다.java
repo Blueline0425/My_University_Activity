4497ebae336c84b6d59527ae9bb740a684c8d016
Blueline0425 <83590303+Blueline0425@users.noreply.github.com>
2023-05-31 23:15:35 +0900

    Create 25314 코딩은 체육과목 입니다.java

---
+++ UNIV_FRESHMAN_ASSIGNMENTS/BaekJoon/단계별로풀어보기/반복문/25314 코딩은 체육과목 입니다.java
@@ -0,0 +1,14 @@
+import static java.lang.System.out;
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		for(int i=0;i<n;i+=4){
+		    out.printf("long ");
+		}
+		out.printf("int");
+	}
+}
