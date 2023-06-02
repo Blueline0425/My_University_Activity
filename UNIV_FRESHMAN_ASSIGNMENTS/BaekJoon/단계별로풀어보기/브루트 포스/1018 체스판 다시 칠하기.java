2a59d104368dd8b35262f46ae02d76fa516e2a8d
Blueline0425 <83590303+Blueline0425@users.noreply.github.com>
2023-06-02 19:18:38 +0900

    Update 1018 체스판 다시 칠하기.java

--- UNIV_FRESHMAN_ASSIGNMENTS/BaekJoon/단계별로풀어보기/브루트 포스/1018 체스판 다시 칠하기.java
+++ UNIV_FRESHMAN_ASSIGNMENTS/BaekJoon/단계별로풀어보기/브루트 포스/1018 체스판 다시 칠하기.java
@@ -7,7 +7,29 @@ public class Main
 		Scanner scanner = new Scanner(System.in);
 		int n = scanner.nextInt();
 		int m = scanner.nextInt();
+		int white_match, black_match, 
+		int[]atch = new int[(n-8)*(m-8)];
 		int[][] narr=new int[n][m];
+		int[][] chess_b={{1,0,1,0,1,0,1,0},
+		                 {0,1,0,1,0,1,0,1},
+		                 {1,0,1,0,1,0,1,0},
+		                 {0,1,0,1,0,1,0,1},
+		                 {1,0,1,0,1,0,1,0},
+		                 {0,1,0,1,0,1,0,1},
+		                 {1,0,1,0,1,0,1,0},
+		                 {0,1,0,1,0,1,0,1},
+		}
+		int[][] chess_w={{0,1,0,1,0,1,0,1},
+		                 {1,0,1,0,1,0,1,0},
+		                 {0,1,0,1,0,1,0,1},
+		                 {1,0,1,0,1,0,1,0},
+		                 {0,1,0,1,0,1,0,1},
+		                 {1,0,1,0,1,0,1,0},
+		                 {0,1,0,1,0,1,0,1},
+		                 {1,0,1,0,1,0,1,0},
+		                
+		}
+		int[][] chess_match=new int[8][8];
 		for(int i=0;i<n;i++){
 		    String str = scanner.nextLine();
 		    for(int k=0;k<m;k++){
@@ -18,6 +40,28 @@ public class Main
 		    }
 		}
 		
+		for(int i=0;i<n-8;i++){//n*m 보드판에서 8*8 chess_w와 chess_b의 0과 1 매칭률 비교 후 매칭률의 최대값을 구하는 식 작성.
+		  //비교횟수는 n-8 * m-8
+		  for(int k=0;k<m-8;k++){
+		      if(narr[i][l]==0){
+		                    for(int n=0;n<8;n++){//체스판 비교
+		                        for(int l=0;l<8;l++){
+		                            if(narr[i+n][k+l]!=chess_w[n][l]){
+		                                
+		                            }
+		                        }
+		                     }
+		              
+		              else{
+		                  for(int n=0;n<8;n++){//체스판 비교
+		                        for(int l=0;l<8;l++){
+		              
+		                        }
+		                  }
+		      
+		      }
+		  }
+		}
 		
 		
 	}
