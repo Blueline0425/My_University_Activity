add8e4d29e6cc0c73081807e65597580c030b579
Blueline0425 <83590303+Blueline0425@users.noreply.github.com>
2023-06-02 23:44:09 +0900

    Update 1018 체스판 다시 칠하기.java

--- UNIV_FRESHMAN_ASSIGNMENTS/BaekJoon/단계별로풀어보기/브루트 포스/1018 체스판 다시 칠하기.java
+++ UNIV_FRESHMAN_ASSIGNMENTS/BaekJoon/단계별로풀어보기/브루트 포스/1018 체스판 다시 칠하기.java
@@ -7,9 +7,10 @@ public class Main
 		Scanner scanner = new Scanner(System.in);
 		int n = scanner.nextInt();
 		int m = scanner.nextInt();
-		int[] narr_Nmatch = new int[(n-7)*(m-7)];
+		int[][] narr_Nmatch = new int[(n-7)*(m-7)][2];
 		for(int i=0;i<(n-7)*(m-7);i++){
-		    narr_Nmatch[i]=0;
+		    for(int k=0;k<2;k++)
+		        narr_Nmatch[i][k]=0;
 		}
 		int[][] narr=new int[n][m];
 		int[][] chess_b={{1,0,1,0,1,0,1,0},
@@ -34,7 +35,7 @@ public class Main
 		
 		for(int i=0;i<n;i++){
 		    String str = new String();
-		    str = scanner.nextLine();
+		    str = scanner.next();
 		    String[] A = str.split("");
 		    for(int k=0;k<m;k++){
 		        if(A[k].equals("W"))//white는 0으로 black은 1로 치환 후 보드판 비교 후 0과 1의 비율이 최대한 높을 때가 최적의 수 
@@ -48,24 +49,32 @@ public class Main
 		
 		
 		
-		int count=0;
+	int count=0;
 		for(int i=0;i<n-7;i++){//n*m 보드판에서 8*8 chess_w와 chess_b의 0과 1 매칭률 비교 후 매칭률의 최대값을 구하는 식 작성.
 		  //비교횟수는 n-7 * m-7
 		  for(int k=0;k<m-7;k++){
-		      if(narr[i][k]==0){
+		      int w=0;
+		      int b=0;
+		      if(narr[i][k]==0){//w일때
 		                    for(int a=0;a<8;a++){//체스판 비교
 		                        for(int l=0;l<8;l++){
-		                            if(narr[i+a][k+l]!=chess_w[a][l]){
-		                                narr_Nmatch[count]++;
+		                            if(narr[i+a][k+l]!=chess_w[a][l]){//w==b일때
+		                                narr_Nmatch[count][0]++;
+		                            }
+		                            if(narr[i+a][k+l]!=chess_b[a][l]){//w==w일때
+		                                narr_Nmatch[count][1]++;
 		                            }
 		                        }
 		                     }
 		    }
-		    else{
+		    else{//b일때
 		                  for(int a=0;a<8;a++){//체스판 비교
 		                        for(int l=0;l<8;l++){
-		                            if(narr[i+a][k+l]!=chess_b[a][l]){
-		                                narr_Nmatch[count]++;
+		                            if(narr[i+a][k+l]!=chess_b[a][l]){//b==w일때
+		                                narr_Nmatch[count][0]++;
+		                            }
+		                            if(narr[i+a][k+l]!=chess_w[a][l]){//b==b일때
+		                                narr_Nmatch[count][1]++;
 		                            }
 		                        }
 		                     }
@@ -74,14 +83,14 @@ public class Main
 		  count++;
 		}
 		
-		
-	}
+		}
 	int chess_min=99999;
-	for(int i : narr_Nmatch){
-	    if(i<chess_min)
-	        chess_min=i;
-	}
-	
+	for(int f=0;f<(n-7)*(m-7);f++){
+		    for(int k=0;k<2;k++)
+		        if(narr_Nmatch[f][k]<chess_min)
+	                chess_min=narr_Nmatch[f][k];
+		}
 	out.println(chess_min);
-    }
+    
+}
 }
