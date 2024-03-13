bf965da9d8804e624a8f5ed0b0ae02576cd5dbe5
Woongjae <83590303+Blueline0425@users.noreply.github.com>
2024-03-13 22:40:51 +0900

    Create 10171 고양이.java

---
+++ Baekjoon/단계별로풀어보기/입출력과사칙연산/10171 고양이.java
@@ -0,0 +1,14 @@
+import static java.lang.System.out;
+import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+public class Main
+{
+	public static void main(String[] args) {
+		
+		    out.printf("\\    /\\%n");
+		    out.printf(" )  ( ')%n");
+		    out.printf("(  /  )%n");
+		    out.printf(" \\(__)|%n");
+    }
+}
