02225ff7a183df841c27e5921451c2b6104a77c6
Woongjae <83590303+Blueline0425@users.noreply.github.com>
2024-03-14 00:11:24 +0900

    Create 2743 단어 길이 재기.java

---
+++ Baekjoon/단계별로풀어보기/문자열/2743 단어 길이 재기.java
@@ -0,0 +1,15 @@
+import static java.lang.System.out;
+import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+public class Main
+{
+	public static void main(String[] args) {
+		
+		Scanner put = new Scanner(System.in);
+		String str=put.next();
+		
+		out.println(str.length());
+    scanner.close():
+    }
+}
