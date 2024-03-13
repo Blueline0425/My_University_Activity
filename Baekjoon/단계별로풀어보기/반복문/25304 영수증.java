02f68d8c3a77e21aa3dbe4b9dce629c536dd1d83
Woongjae <83590303+Blueline0425@users.noreply.github.com>
2024-03-14 00:01:06 +0900

    Create 25304 영수증.java

---
+++ Baekjoon/단계별로풀어보기/반복문/25304 영수증.java
@@ -0,0 +1,23 @@
+import static java.lang.System.out;
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		int a,b;
+		int sum=0;
+		Scanner scanner = new Scanner(System.in);
+		int x = scanner.nextInt();
+		int n = scanner.nextInt();
+		for(int i=0;i<n;i++){
+		    a = scanner.nextInt();
+		    b = scanner.nextInt();
+		    sum+=a*b;
+		}
+		
+		if(x==sum)
+		    out.println("Yes");
+		else
+		    out.println("No");
+    scanner.close();
+	}
+}
