e2707d9afcd6016480ac8be9b5f51b427d18a575
Woongjae <83590303+Blueline0425@users.noreply.github.com>
2024-03-14 00:10:58 +0900

    Create 27866 문자와 문자열.java

---
+++ Baekjoon/단계별로풀어보기/문자열/27866 문자와 문자열.java
@@ -0,0 +1,16 @@
+import static java.lang.System.out;
+import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+public class Main
+{
+	public static void main(String[] args) {
+		
+		Scanner put = new Scanner(System.in);
+		String str=put.next();
+		int n=put.nextInt();
+		
+		out.println(str.charAt(n-1));
+    scanner.close():
+    }
+}
