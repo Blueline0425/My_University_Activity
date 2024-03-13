0180f8d0bd3fd8ace83bacb5687ecfa1f17b1ffe
Woongjae <83590303+Blueline0425@users.noreply.github.com>
2024-03-13 22:40:12 +0900

    Create 11382 꼬마 정민.java

---
+++ Baekjoon/단계별로풀어보기/입출력과사칙연산/11382 꼬마 정민.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    Scanner scanner = new Scanner(System.in);
+	    Long a = scanner.nextLong();
+	    Long b = scanner.nextLong();
+	    Long c = scanner.nextLong();
+	    System.out.println(a+b+c);
+      Scanner.close():
+	}
+}
