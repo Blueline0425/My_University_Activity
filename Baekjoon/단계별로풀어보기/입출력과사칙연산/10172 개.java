3f792f5f24ab35329df4cd24ba1806003e6bc450
Woongjae <83590303+Blueline0425@users.noreply.github.com>
2024-03-13 22:41:24 +0900

    Create 10172 개.java

---
+++ Baekjoon/단계별로풀어보기/입출력과사칙연산/10172 개.java
@@ -0,0 +1,15 @@
+import static java.lang.System.out;
+import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+public class Main
+{
+	public static void main(String[] args) {
+		
+		    out.printf("|\\_/|%n");
+		    out.printf("|q p|   /}%n");
+		    out.printf("( 0 )\"\"\"\\%n");
+		    out.printf("|\"^\"`    |%n");
+		    out.printf("||_/=\\\\__|%n");
+    }
+}
