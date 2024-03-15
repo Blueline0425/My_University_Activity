5145e617cab01be17d86afa8ff7282df79654761
Woongjae <83590303+Blueline0425@users.noreply.github.com>
2024-03-15 23:59:05 +0900

    Create 11718 그대로 출력하기.java

---
+++ Baekjoon/단계별로풀어보기/문자열/11718 그대로 출력하기.java
@@ -0,0 +1,14 @@
+import static java.lang.System.out;
+import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+public class Main
+{
+	public static void main(String[] args) {
+		
+		Scanner put = new Scanner(System.in);
+		while(put.hasNext()){
+		    out.println(put.nextLine());
+		}
+    }
+}
