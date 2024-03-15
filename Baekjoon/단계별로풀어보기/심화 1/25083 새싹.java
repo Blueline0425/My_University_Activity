ff54fa1af217b227c3160fa341fde5bf3733384d
Woongjae <83590303+Blueline0425@users.noreply.github.com>
2024-03-16 00:05:05 +0900

    Create 25083 새싹.java

---
+++ Baekjoon/단계별로풀어보기/심화 1/25083 새싹.java
@@ -0,0 +1,17 @@
+import static java.lang.System.out;
+import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+public class Main
+{
+	public static void main(String[] args) {
+		
+		    out.printf("         ,r'\"7%n");
+		    out.printf("r`-_   ,'  ,/%n");
+		    out.printf(" \\. \". L_r'%n");
+		    out.printf("   `~\\/%n");
+		    out.printf("      |%n");
+            out.printf("      |%n");
+	    
+	}
+}
