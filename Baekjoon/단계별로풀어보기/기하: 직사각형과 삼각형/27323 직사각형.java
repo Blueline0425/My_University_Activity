9adaf63561a9be0a15e0ff718f120a1b1b6b55cc
Woongjae <83590303+Blueline0425@users.noreply.github.com>
2024-03-16 00:53:54 +0900

    Create 27323 직사각형.java

---
+++ Baekjoon/단계별로풀어보기/기하: 직사각형과 삼각형/27323 직사각형.java
@@ -0,0 +1,11 @@
+import static java.lang.System.out;
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int a = input.nextInt();
+		int b = input.nextInt();
+		out.println(a*b);
+	}
+}
