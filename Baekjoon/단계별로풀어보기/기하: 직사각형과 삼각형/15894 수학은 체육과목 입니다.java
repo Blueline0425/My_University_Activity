78ef2a9242b68bf93f25773d2387ada59cd8f85d
Woongjae <83590303+Blueline0425@users.noreply.github.com>
2024-03-16 00:54:47 +0900

    Create 15894 수학은 체육과목 입니다.java

---
+++ Baekjoon/단계별로풀어보기/기하: 직사각형과 삼각형/15894 수학은 체육과목 입니다.java
@@ -0,0 +1,10 @@
+import static java.lang.System.out;
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		    long x = input.nextLong();
+		out.println(x*4);
+	}
+}
